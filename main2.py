c1cf08bc103d4b92659141dbf2a157bbdd0f1140
LabServices <labservices@MEM3245-0243.local>
2022-07-28 12:07:57 -0400

    Updated Main

--- main2.py
+++ main2.py
@@ -1,25 +1,50 @@
-
 import pygame
+import random
+
 # Initialize the pygame
 pygame.init()
 # screen
 screen = pygame.display.set_mode((800, 600))
+
 # Title
 pygame.display.set_caption("Long Night of Solace")
 icon = pygame.image.load('elite2.png')
 pygame.display.set_icon(icon)
+
 # player
 playerimg = pygame.image.load('spaceship2.png')
 playerx = 370
 playery = 480
 playerx_change = 0
+
+# banshee
+alienimg = pygame.image.load('banshee.png')
+alienx = random.randint (0,800)
+alieny = random.randint(50,150)
+alienx_change = -0.2
+alieny_change = 40
+
+
 def player(x, y):
     screen.blit(playerimg, (x, y))
+
+
+def alien(x, y):
+    screen.blit(alienimg, (x, y))
+
 # game loop
 running = True
 while running:
-    screen.fill((19, 0, 26))
+    screen.fill((1, 41, 54))
     player(playerx, playery)
+    alien(alienx, alieny)
+    alienx += alienx_change
+    if alienx <= 0:
+        alienx_change = 0.2
+        alieny += alieny_change
+    elif alienx >= 736:
+        alienx_change = -0.2
+        alieny += alieny_change
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
